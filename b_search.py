7870dcf5ec5c51fc1f295b0d5594746b7a7d8d7b
Lankymate <91146419+Lankymate@users.noreply.github.com>
2021-09-25 16:31:43 +0300

    Add files via upload

--- b_search.py
+++ b_search.py
@@ -30,39 +30,15 @@ def b_search(train, test, with_correction = True):
         
     train = train.drop(['date', 'data_type'], axis = 1)
     test = test.drop(['date', 'data_type'], axis = 1)
-    def deviation_metric_one_sample(y_true: typing.Union[float, int], y_pred: typing.Union[float, int]) -> float:
-        """
-        Реализация кастомной метрики для хакатона.
-
-        :param y_true: float, реальная цена
-        :param y_pred: float, предсказанная цена
-        :return: float, значение метрики
-        """
-        THRESHOLD = 0.15
-        NEGATIVE_WEIGHT = 1.1
-        deviation = (y_pred - y_true) / np.maximum(1e-8, y_true)
-        if np.abs(deviation) <= THRESHOLD:
-            return 0
-        elif deviation <= - 4 * THRESHOLD:
-            return 9 * NEGATIVE_WEIGHT
-        elif deviation < -THRESHOLD:
-            return NEGATIVE_WEIGHT * ((deviation / THRESHOLD) + 1)**2
-        elif deviation < 4 * THRESHOLD:
-            return ((deviation / THRESHOLD) - 1)**2
-        else:
-            return 9
-
-
-    def deviation_metric(y_true: np.array, y_pred: np.array) -> float:
-        return np.array([deviation_metric_one_sample(y_true[n], y_pred[n]) for n in range(len(y_true))]).mean()
-
+ 
     kf = KFold(n_splits=5, shuffle=True, random_state=42)
     estimator = ctb.CatBoostRegressor(
-        random_state=42
+        random_state=42,
+        verbose = 1000
     )
     opt = BayesSearchCV(
         estimator = estimator,
-        scoring = make_scorer(deviation_metric, greater_is_better=False),
+        scoring = 'neg_mean_absolute_error',
         cv = kf,
         n_iter=500,
         refit = True,
